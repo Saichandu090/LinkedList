be5970ef8ede18422a0d0d92f9af134420c4d38d
Saichandu090 <marrisaichandu143@gmail.com>
2024-09-07 20:26:05 +0530

    Added Singly Linked List Own Impl

---
+++ src/MainSingly.java
@@ -0,0 +1,19 @@
+public class MainSingly
+{
+    public static void main(String[] args) {
+
+        SinglyLinkedList<Integer> st=new SinglyLinkedList<Integer>();
+        System.out.println("Size is : "+st.size());
+        System.out.println(st.delete(0));
+        st.add(52);
+        System.out.println(st.delete(0));
+        st.add(63);
+       // System.out.println(st.deleteFirst());
+        st.add(85);
+        st.add(96);
+        st.add(66);
+        st.addLast(700);
+        System.out.println(st);
+        System.out.println(st.delete(4));
+    }
+}
---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,154 @@
+import javax.print.attribute.standard.JobKOctets;
+
+public class SinglyLinkedList<Object>
+{
+    class Node
+    {
+        Object data;
+        Node next;
+        Node(Object data)
+        {
+            this.data=data;
+        }
+    }
+    Node head;
+
+    public void addFirst(Object data)
+    {
+        Node n=new Node(data);
+        n.next=head;
+        head=n;
+    }
+
+    public void add(Object data,int index)
+    {
+        Node n=new Node(data);
+        if(index==0)
+            addFirst(data);
+        Node temp=head;
+        while(temp!=null&&index>1)
+        {
+            temp=temp.next;
+            index--;
+        }
+        if(temp!=null)
+        {
+            n.next = temp.next;
+            temp.next = n;
+        }
+        else
+            System.out.println("Index not in range!!");
+    }
+
+    public void addLast(Object data)
+    {
+        Node n=new Node(data);
+        Node temp=head;
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+        }
+        temp.next=n;
+    }
+
+    public void add(Object data)
+    {
+        Node n=new Node(data);
+        if(head==null)
+        {
+            head=n;
+            return;
+        }
+        Node temp=head;
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+        }
+        temp.next=n;
+    }
+
+    public Object deleteFirst()
+    {
+        if(head==null)
+            return (Object) "List is Empty!!";
+        Object data=head.data;
+        head=head.next;
+        return data;
+    }
+
+    public Object deleteLast()
+    {
+        if(head==null)
+            return (Object) "List is Empty!!";
+        Node temp=head;
+        Node prev=null;
+        while(temp.next!=null)
+        {
+            prev=temp;
+            temp=temp.next;
+        }
+        Object data=temp.data;
+        if(prev!=null) {
+            prev.next = null;
+        }
+        else {
+            head = null;
+        }
+        return data;
+    }
+
+    public Object delete(int index)
+    {
+        if(index==0)
+        {
+           return deleteFirst();
+        }
+        Node temp=head;
+        Node prev=null;
+        while(temp!=null&&index>0)
+        {
+            prev=temp;
+            if (temp.next!=null)
+                 temp=temp.next;
+            else
+            {
+                 return (Object)"Index out of Range";
+            }
+            index--;
+        }
+        Object data=temp.data;
+        if(temp!=null)
+        {
+            if (prev != null)
+                prev.next = temp.next;
+            temp.next=null;
+        }
+        return data;
+    }
+
+    @Override
+    public String toString()
+    {
+        String st="[";
+        Node temp=head;
+        while(temp!=null)
+        {
+            st=st+temp.data;
+            if(temp.next!=null)
+                st=st+"->";
+            temp=temp.next;
+        }
+        return st+"]";
+    }
+
+    public int size()
+    {
+        return size(head);
+    }
+    public int size(Node temp)
+    {
+        if(temp==null)
+            return 0;
+        return 1+size(temp.next);
+    }
+}
